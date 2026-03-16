51f464d10771a9e79c757038f8314ac98b7f9511
jlima <jlima@example.com>
2026-03-16 12:42:41 -0400

    Remove (Sheets) from button text

--- main.js
+++ main.js
@@ -140,7 +140,7 @@ function renderHome() {
         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
         </svg>
-        Finalizar Jornada y Guardar (Sheets)
+        Finalizar Jornada y Guardar
       </button>
     </div>
     
